28d1e20bb514b98722b9a2cb7395c35c2a2d9b8d
V0D14ka <minecraft35510880@gmail.com>
2024-06-05 10:26:55 -0400

    uploads dir

--- profile.php
+++ profile.php
@@ -84,8 +84,8 @@ if (isset($_POST["submit"])) {
     if(!empty($_FILES['file'])) {
         if (@$_FILES['file']['type'] == 'image/gif' || @$_FILES['file']['type'] == 'image/jpeg' || @$_FILES['file']['type'] == 'image/png' 
         || @$_FILES['file']['type'] == 'image/jpg' || @$_FILES['file']['type'] == 'image/x-png' && $_FILES['file']['size'] < 102400) {
-            move_uploaded_file(@$_FILES['file']['tmp_name'], 'upload/' . $_FILES['file']['name']);
-            echo 'Load in: upload/' . $_FILES['file']['name'];
+            move_uploaded_file(@$_FILES['file']['tmp_name'], 'uploads/' . $_FILES['file']['name']);
+            echo 'Load in: uploads/' . $_FILES['file']['name'];
         } else {
             echo "Incorrect type or size";
         }
