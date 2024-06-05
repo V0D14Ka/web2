5e841a9b5b2e242a7cacffc0ad3cf78d1047cbd9
V0D14ka <minecraft35510880@gmail.com>
2024-06-04 22:58:02 -0400

    db.php fix

--- db.php
+++ db.php
@@ -10,32 +10,32 @@ if (!$link) {
     die('Error via connect: '. mysqli_connect_error());
 }
 
-$sql = 'CREATE DATABASE IF NO EXISTS $dbname';
+$sql = "CREATE DATABASE IF NO EXISTS $dbname";
 
 if (!mysqli_query($link, $sql)) {
-    echo('Error via creating table');
+    echo("Error via creating table");
 }
 
 mysqli_close($link);
 
 $link = mysqli_connect($servername, $username, $password, $dbname);
-$sql = 'CREATE TABLE IF NOT EXISTS users(
+$sql = "CREATE TABLE IF NOT EXISTS users(
     id int not null primary key auto_increment,
     username varchar(15) not null,
     email varchar(30) not null,
-    pass varchar(30) not null)';
+    pass varchar(30) not null)";
 
 if (!mysqli_query($link, $sql)) {
-    echo('Error via creating table users');
+    echo("Error via creating table users");
 }
 
-$sql = 'CREATE TABLE IF NOT EXISTS posts(
+$sql = "CREATE TABLE IF NOT EXISTS posts(
     id int not null primary key auto_increment,
     title varchar(30) not null,
-    content varchar(500) not null)';
+    content varchar(500) not null)";
 
 if (!mysqli_query($link, $sql)) {
-    echo('Error via creating table posts');
+    echo("Error via creating table posts");
 }
 
 mysqli_close($link);
