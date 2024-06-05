3c0fd2559c4fa04605e304f8a7e1e5af5d6f4e32
V0D14ka <minecraft35510880@gmail.com>
2024-06-05 00:53:25 -0400

    reg

---
+++ registration.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Yashin V.S</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Регистрация </h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <form action="registation.php" method="POST">
+                    <div class="row form-reg">
+                        <input type="email" class="form" name="Email">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
