3893ac962005dbbdfd5b4d456fa4067dc28da314
V0D14ka <minecraft35510880@gmail.com>
2024-06-05 01:15:58 -0400

    reg

--- registration.php
+++ registration.php
@@ -19,7 +19,7 @@
         </div>
         <div class="row">
             <div class="col-12">
-                <form action="registation.php" method="POST">
+                <form action="registration.php" method="POST">
                     <div class="row form-reg"><input type="email" class="form" name="email" placeholder="Email"></div>
                     <div class="row form-reg"><input type="text" class="form" name="login" placeholder="Login" ></div>
                     <div class="row form-reg"><input type="password" class="form" name="password" placeholder="Password"></div>
@@ -31,3 +31,24 @@
 </body>
 
 </html>
+
+<?php
+require_once "db.php";
+$link = mysqli_connect("127.0.0.1","root","root","web2");
+
+if (isset($_POST["submit"])) {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $login = $_POST["login"];
+
+    if (!$email || !$password || !$login) {
+        die("Please enter correct data");
+    }
+    $sql = "insert into users values ('$email', '$username', '$password')";
+
+    if (mysqli_query( $link, $sql)) {
+        echo "Can't add user";
+    }
+}
+
+?>
